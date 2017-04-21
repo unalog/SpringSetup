ef08d7af63d5b92805b9652faf4cc24dd7cbe1b9
una <una@ubivelox.com>
2017-04-21 18:41:06 +0900

    Board test

--- src/main/java/org/una/persistence/BoardDAO.java
+++ src/main/java/org/una/persistence/BoardDAO.java
@@ -1,5 +1,19 @@
 package org.una.persistence;
 
-public class BoardDAO {
+import java.util.List;
+
+import org.una.domain.BoardVO;
+
+public interface BoardDAO {
+	
+	public void create(BoardVO vo) throws Exception;
+	
+	public BoardVO read (Integer bno) throws Exception;
+	
+	public void update (BoardVO vo) throws Exception;
+	
+	public void delete (Integer bno) throws Exception;
+	
+	public List<BoardVO>listAll() throws Exception;
 
 }
