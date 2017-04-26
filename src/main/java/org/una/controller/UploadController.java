72c9d5974868b1503acd2afb649d87c7d6b5438a
una <una@ubivelox.com>
2017-04-26 18:20:03 +0900

    add ajax upload

--- src/main/java/org/una/controller/UploadController.java
+++ src/main/java/org/una/controller/UploadController.java
@@ -1,19 +1,77 @@
 package org.una.controller;
 
+import java.io.File;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.una.util.UploadFileUtils;
 
 @Controller
 public class UploadController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 	
+	@Resource
+	private String uploadPath;
+	
 	@RequestMapping(value="/uploadForm", method = RequestMethod.GET)
 	public void uploadForm()
 	{
 		
 	}
+	
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
+	public String uploadForm(MultipartFile file, Model model) throws Exception{
+		
+		logger.info("orinalName: "+file.getOriginalFilename());
+		logger.info("size" + file.getSize());
+		logger.info("contentType : "+ file.getContentType() );
+		
+		String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		
+		model.addAttribute("savedName", savedName);
+		
+		return "uploadResult";
+	}
+	
+	private String uploadFile(String originName, byte[] fileData) throws Exception{
+		
+		UUID uid = UUID.randomUUID();
+		
+		String savedName = uid.toString() + "_" + originName;
+		
+		File target = new File(uploadPath, savedName);
+		FileCopyUtils.copy(fileData, target);
+		
+		return savedName;
+	}
+	
+	@RequestMapping(value="/uploadAjax", method = RequestMethod.GET)
+	public void uploadAjax(){
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/uploadAjax", method = RequestMethod.POST, produces="text/plain;charset=UTF-8")
+	public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception{
+		
+		logger.info("originalName: "+ file.getOriginalFilename());
+		logger.info("size" + file.getSize());
+		logger.info("contentType :"+file.getContentType());
+		
+		return new ResponseEntity<>(UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), HttpStatus.CREATED);
+	}
+	
 }
--- src/main/webapp/WEB-INF/views/uploadForm.jsp
+++ src/main/webapp/WEB-INF/views/uploadForm.jsp
@@ -8,6 +8,12 @@
 <title>Insert title here</title>
 
 <style>
+
+iframe {
+	width:  0px;
+	height: 0px;
+	border: 0px
+}
 </style>
 </head>
 <body>
@@ -18,5 +24,14 @@
 		<input type="file" name="file"> <input type="submit">
 
 	</form>
+	
+	<iframe name="zeroFrame"> </iframe>
+<script>
+	function addFilePath(msg) {
+		alert(msg);
+		document.getElementById("form1").reset();
+	}
+
+</script>
 </body>
 </html>
